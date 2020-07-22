5d3f50f52d79915c7884be5be1d27c80c7483036
Koushik Mohan <koushikmohan1996@gmail.com>
2020-07-22 22:35:08 +0530

    Update sample.test.js

--- sample/sample.test.js
+++ sample/sample.test.js
@@ -4,4 +4,7 @@ describe('Hello', () => {
   test('Should say hello with passed parameter', () => {
     expect(hello('Koushik')).toBe('Hello Koushik');
   });
+  test('Always pass', () => {
+    expect(true)).toBe(true);
+  });
 });
